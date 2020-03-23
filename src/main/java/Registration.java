26d9526c96f8878a6e00449519da2b76be83dff4
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-23 20:24:27 +0530

    Added:EmailValidation

--- src/main/java/Registration.java
+++ src/main/java/Registration.java
@@ -2,9 +2,13 @@ import java.util.regex.Pattern;
 
 public class Registration {
     String defaultPattern="^[A-Z][a-zA-Z]{2,}$";
+    String emailPattern="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
 
     public boolean checkName(String Abhishek) {
 
         return Pattern.matches(defaultPattern,Abhishek);
+    }
+    public boolean checkEmail(String email) {
+        return Pattern.matches(emailPattern,email);
     }
 }
--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -3,28 +3,36 @@ import org.junit.Test;
 
 
 public class RegistrationTest {
-   Registration obj = new Registration();
-   @Test
-   public void givenfirstName_whenValid_thenTrue() {
-       boolean firstname=obj.checkName("Abhishek");
-       Assert.assertTrue(firstname);
-   }
+    Registration obj = new Registration();
+
+    @Test
+    public void givenfirstName_whenValid_thenTrue() {
+        boolean firstname = obj.checkName("Abhishek");
+        Assert.assertTrue(firstname);
+    }
 
     @Test
     public void givenfirstName_whenInValid_thenFalse() {
-        boolean firstname=obj.checkName("Abhishek");
+        boolean firstname = obj.checkName("Abhishek");
         Assert.assertFalse(firstname);
     }
 
     @Test
     public void givenlastName_whenValid_thenTrue() {
-        boolean lastname=obj.checkName("Abhishek");
+        boolean lastname = obj.checkName("Abhishek");
         Assert.assertTrue(lastname);
     }
+
     @Test
     public void givenlastName_wheninValid_thenFalse() {
-        boolean lastname=obj.checkName("Abhishek");
+        boolean lastname = obj.checkName("Abhishek");
         Assert.assertFalse(lastname);
     }
-}
 
+
+    @Test
+    public void givenEmail_whenValid_thenTrue() {
+        boolean email = obj.checkEmail("rishibodake@gmail.com");
+        Assert.assertTrue(email);
+    }
+}
